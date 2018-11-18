cd463c29c3b0c995534bd60860a042cc67520c1a
Sam Wilkins <samwilkins333@gmail.com>
2018-11-18 10:57:23 -0500

    Initial commit

--- wp-content/themes/bpr2018/resources/templates/layout/footer.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/layout/footer.tpl.php
@@ -1,9 +1,52 @@
             <footer class="footer">
-                <ul>
-                    <li><a href="<?= get_home_url(); ?>">Index</a></li>
-                    <li><a href="https://github.com/tonik/tonik/wiki">Documentation</a></li>
-                    <li><a href="https://github.com/tonik/tonik">Github</a></li>
-                </ul>
+              <ul>
+                  <li><a href="<?= get_home_url(); ?>">Index</a></li>
+                  <li><a href="https://github.com/tonik/tonik/wiki">Documentation</a></li>
+                  <li><a href="https://github.com/tonik/tonik">Github</a></li>
+              </ul>
+              <div id="background">
+                <div class="content-wrapper container-fluid">
+                  <div class="row">
+                    <div class="content col-lg-3">
+                      <div>
+                        <img class="top_element" id="logo" src="http://www.brownpoliticalreview.org/wp-content/uploads/2016/03/SQ_Logo.jpg" width="25%"/>
+                      </div>
+                      <div>
+                        <ul id="media_links">
+                          <li><a target="_blank" href="https://www.facebook.com/BrownPoliticalReview">F</a></li>
+                          <li><a target="_blank" href="https://twitter.com/BrownBPR">t</a></li>
+                          <li><a target="_blank" href="https://www.youtube.com/user/BrownBPR/videos">y</a></li>
+                          <li><a target="_blank" href="https://www.instagram.com/brownpoliticalreview/">i</a></li>
+                          <li><a target="_blank" href="https://soundcloud.com/bpr-chief-of-staff">s</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="content col-lg-3">
+                      <div class="internal_links">
+                        <p>Brown Political Review</p>
+                        <hr/>
+                        <ul>
+                          <li><a href="https://www.brownpoliticalreview.org/about">About</a></li>
+                          <li><a href="https://www.brownpoliticalreview.org/masthead">Masthead</a></li>
+                          <li><a href="https://www.brownpoliticalreview.org/contact-us">Contact Us</a></li>
+                          <li><a href="https://www.brownpoliticalreview.org/withus">Join BPR!</a></li>
+                          <li><a href="https://www.brownpoliticalreview.org/donate">Donate</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="content col-lg-3">
+                      <div class="tags">
+                        <p>Tags</p>
+                        <hr/>
+                        <select class="category">
+
+                        </select>
+                      </div>
+                    </div>
+                    <div class="content col-lg-3"></div>
+                  </div>
+                </div>
+              </div>
             </footer>
         </main>
 
