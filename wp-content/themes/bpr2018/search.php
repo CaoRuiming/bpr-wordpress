5508e1befe862889c006a8a01ecd3dfddce42deb
Yusuf Karim <ykarim250@gmail.com>
2019-02-08 14:52:29 -0500

    Added files for author and search (#2)
    * Added files for author and search
    
    * Fix author and search directory

---
+++ wp-content/themes/bpr2018/search.php
@@ -0,0 +1,13 @@
+<?php
+/*!
+	Template Name: Search Results Page
+	Template Post Type: page
+*/
+namespace BPRWP\Theme\Search;
+use function BPRWP\Theme\App\template;
+/**
+ * Renders search results page.
+ *
+ * @see resources/templates/search.tpl.php
+ */
+template('search');
