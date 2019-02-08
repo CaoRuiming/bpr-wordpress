73e5b4262e6142d087de4818783b42a5709cbb93
CaoRuiming <rcao22556@gmail.com>
2019-02-08 14:28:47 -0500

    added files for front page, comments

---
+++ wp-content/themes/bpr2018/comments.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BPRWP\Theme\Comments;
+
+/*
+|------------------------------------------------------------------
+| Comments Controller
+|------------------------------------------------------------------
+|
+| The template controller for displaying website comments section,
+|
+*/
+
+use function BPRWP\Theme\App\template;
+
+/**
+ * Renders comments.
+ *
+ * @see resources/templates/partials/comments.tpl.php
+ */
+template('partials/comments');
---
+++ wp-content/themes/bpr2018/front-page.php
@@ -0,0 +1,18 @@
+<?php
+
+/*!
+	Template Name: Front Page
+	Template Post Type: page
+*/
+
+namespace BPRWP\Theme\FrontPage;
+
+
+use function BPRWP\Theme\App\template;
+
+/**
+ * Renders front page.
+ *
+ * @see resources/templates/front-page.tpl.php
+ */
+template('front-page');
