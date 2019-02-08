4cf0fa3aae03d3dba002754526fd3b5c7da8cbf7
Yusuf Karim <ykarim250@gmail.com>
2019-02-08 14:44:56 -0500

    Added files for author and search

---
+++ wp-content/themes/bpr2018/author.php
@@ -0,0 +1,13 @@
+<?php
+/*!
+	Template Name: Author Page
+	Template Post Type: page
+*/
+namespace BPRWP\Theme\Author;
+use function BPRWP\Theme\App\template;
+/**
+ * Renders author's page.
+ *
+ * @see resources/templates/author.tpl.php
+ */
+template('author');
