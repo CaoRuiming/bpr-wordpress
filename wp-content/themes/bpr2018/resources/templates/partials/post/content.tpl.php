0eb38270fffaab42cd5b6cb13d1247b4dd54ef43
CaoRuiming <rcao22556@gmail.com>
2019-04-05 13:46:13 -0400

    single post template has correct fonts, basic layout

--- wp-content/themes/bpr2018/resources/templates/partials/post/content.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/partials/post/content.tpl.php
@@ -1,9 +1,38 @@
 <article>
-    <h1><?php the_title(); ?></h1>
+    <div class="row">
+        <div class="col-lg-1"></div> <!-- Paddging -->
 
-    <time>
-        <small><?php the_date(); ?></small>
-    </time>
+        <div class="content col-lg-10">
+            <h1><?php the_title(); ?></h1>
 
-    <p><?php the_content(); ?></p>
+            <div class="byline">
+                <?php the_author(); ?> | <?php the_date(); ?>
+            </div>
+
+            <?php 
+            $url = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));
+            if ($url):
+            ?>
+                <img class="featured-image" src="<?php echo $url; ?>">
+            <?php endif; ?>
+
+            <div class="hline"></div>
+        </div>
+
+        <div class="col-lg-1"></div> <!-- Paddging -->
+    </div>
+
+    <div class="row">
+        <div class="col-lg-1">
+            Part of some issue
+        </div>
+
+        <div class="content col-lg-10">
+            <p><?php the_content(); ?></p>
+        </div>
+
+        <div class="col-lg-1 post-category">
+            <?php the_category(); ?>
+        </div>
+    </div>
 </article>
--- wp-content/themes/bpr2018/resources/templates/single.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/single.tpl.php
@@ -1,24 +1,18 @@
 <?php get_header(); ?>
 
 <div class="single-template content-wrapper container fluid">
-    <div class="row">
-        <div class="col-lg-2"></div>
-        <div class="content col-lg-8">
-            <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post() ?>
-            <?php
-                        /**
-                         * Functions hooked into `theme/single/content` action.
-                         *
-                         * @hooked BPRWP\Theme\App\Structure\render_post_content - 10
-                         */
-                        do_action('theme/single/content');
-                        ?>
-                <?php endwhile; ?>
-                <?php endif; ?>
-        </div>
-        <div class="col-lg-2"></div>
-    </div>
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post() ?>
+    <?php
+                /**
+                 * Functions hooked into `theme/single/content` action.
+                 *
+                 * @hooked BPRWP\Theme\App\Structure\render_post_content - 10
+                 */
+                do_action('theme/single/content');
+                ?>
+        <?php endwhile; ?>
+        <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
