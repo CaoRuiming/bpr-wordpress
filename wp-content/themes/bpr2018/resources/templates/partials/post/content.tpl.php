5a145af437c8b073efd43460a0679ac3376eb53a
CaoRuiming <rcao22556@gmail.com>
2019-04-19 19:36:11 -0400

    added author bio to end of article template

--- wp-content/themes/bpr2018/resources/templates/partials/post/content.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/partials/post/content.tpl.php
@@ -3,11 +3,13 @@
         <div class="col-lg-1"></div> <!-- Padding -->
 
         <div class="content col-lg-10">
-            <a name="article-top"></a>
+            <a id="article-top" name="article-top"></a> <!-- For back to top -->
             <h1><?php the_title(); ?></h1>
 
             <div class="byline">
-                <?php the_author(); ?> | <?php the_date(); ?>
+                <a href="<?php get_author_posts_url(get_the_author_meta('ID')); ?>">
+                    <?php the_author(); ?>
+                </a> | <?php the_date(); ?>
             </div>
 
             <?php 
@@ -30,8 +32,33 @@
 
         <div class="content col-lg-10">
             <p><?php the_content(); ?></p>
+
             <div class="back-to-top">
                 <a href="#article-top">Back to Top</a>
+            </div>
+
+            <div class="article-bottom-wrapper container-fluid">
+                <div class="article-bottom row">
+                    <div class="tags-and-author col-sm-9">
+                        <div class="article-tags">
+                            <?php 
+                            $tags = wp_get_post_tags(get_the_ID());
+                            foreach($tags as $tag) {
+                                echo '<div class="tag">'. $tag->name . '</div>';
+                            }
+                            ?>
+                        </div>
+
+                        <div class="about-author">
+                            <p class="about-author-title">About the Author</p>
+                            <p class="about-author-bio">
+                                <?php echo get_the_author_meta('user_description'); ?>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="social-and-sharing col-sm-3"></div>
+                </div>
             </div>
         </div>
 
