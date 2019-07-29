a9815208dab907f9718e17117c30ff814f5d5a9a
CaoRuiming <rcao22556@gmail.com>
2019-07-29 19:53:04 -0400

    cleaned up footer

--- wp-content/themes/bpr2018/resources/templates/layout/footer.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/layout/footer.tpl.php
@@ -1,45 +1,13 @@
         </main>
-        <footer class="footer">
-          <div id="background">
-            <div class="content-wrapper container-fluid">
+        <footer>
+          <div id="footer-background">
+            <div id="footer-content" class="container-fluid">
               <div class="row">
                 <div class="footer-heading content col-lg-3">
-                  <div class="footer-logo" style="background-image: url(<?php echo get_template_directory_uri() . '/resources/assets/images/BPR_logo_white.png' ?>)"></div>
+                  <div class="logo" style="background-image: url(<?php echo get_template_directory_uri() . '/resources/assets/images/BPR_logo_white.png' ?>)"></div>
                 </div>
-                <div class="content col-lg-3">
-                  <!-- <div id="media_links">
-                      <?php wp_nav_menu( array(
-                          'menu' => 'social-media',
-                          // 'theme_location' => 'top',
-                          'menu_id' => '4',
-                      ) ); ?>
-                  </div> -->
-                </div>
-                <div class="content col-lg-3">
-                  <p id="sub_heading">Brown Political Review</p>
-                  <hr/>
-                  <div class="internal_links left">
-                    <?php wp_nav_menu( array(
-                        'menu' => 'footer-menu',
-                        // 'theme_location' => 'top',
-                        'menu_id' => 'footer-menu',
-                    ) ); ?>
-                  </div>
-                  <div class="internal_links center">
-                    <?php wp_nav_menu( array(
-                        'menu' => 'footer-menu',
-                        // 'theme_location' => 'top',
-                        'menu_id' => 'footer-menu',
-                    ) ); ?>
-                  </div>
-                  <div class="internal_links right">
-                    <?php wp_nav_menu( array(
-                        'menu' => 'footer-menu',
-                        // 'theme_location' => 'top',
-                        'menu_id' => 'footer-menu',
-                    ) ); ?>
-                  </div>
-                </div>
+                <div class="content col-lg-3"></div>
+                <div class="content col-lg-3"></div>
                 <div class="content col-lg-3"></div>
               </div>
             </div>
--- wp-content/themes/bpr2018/resources/templates/layout/head.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/layout/head.tpl.php
@@ -1,35 +1,35 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
-        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
-        <main id="app" class="app">
-            <nav id="main-menu" class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-                <div class="container">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <a class="navbar-brand" href="<?= get_home_url(); ?>">
-                        <div class="header-logo" style="background-image: url(<?php echo get_template_directory_uri() . '/resources/assets/images/BPR_logo_black.png' ?>)"></div>
-                    </a>
-                        <?php
-                        wp_nav_menu( array(
-                            'theme_location'    => 'primary',
-                            'depth'             => 2,
-                            'container'         => 'div',
-                            'container_class'   => 'collapse navbar-collapse',
-                            'container_id'      => 'navbar-collapse-1',
-                            'menu_class'        => 'nav navbar-nav',
-                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                            'walker'            => new WP_Bootstrap_Navwalker(),
-                        ) );
-                        ?>
-                    </div>
-                </nav>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <main id="app" class="app">
+      <nav id="main-menu" class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="<?= get_home_url(); ?>">
+            <div class="logo" style="background-image: url(<?php echo get_template_directory_uri() . '/resources/assets/images/BPR_logo_black.png' ?>)"></div>
+          </a>
+          <?php
+          wp_nav_menu(array(
+            'theme_location'    => 'primary',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'navbar-collapse-1',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+          ));
+          ?>
+        </div>
+      </nav>
