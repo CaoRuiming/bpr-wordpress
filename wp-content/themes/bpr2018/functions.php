901d3b1e3c6e89a300a0f7c6a88bd0156f566777
Nick Young <youngnicholas228@gmail.com>
2020-02-15 20:10:53 -0500

    Finished comment styling

--- wp-content/themes/bpr2018/functions.php
+++ wp-content/themes/bpr2018/functions.php
@@ -89,3 +89,23 @@ require_once __DIR__ . '/breadcrumbs.php' ;
 
 // For enabling pagination in category template
 ini_set('mysql.trace_mode', 0);
+
+// change comment form fields order
+add_filter( 'comment_form_fields', 'mo_comment_fields_custom_order' );
+function mo_comment_fields_custom_order( $fields ) {
+	$comment_field = $fields['comment'];
+	$author_field = $fields['author'];
+	$email_field = $fields['email'];
+	$url_field = $fields['url'];
+	unset( $fields['comment'] );
+	unset( $fields['author'] );
+	unset( $fields['email'] );
+	unset( $fields['url'] );
+	// the order of fields is the order below, change it as needed:
+	$fields['author'] = $author_field;
+	$fields['email'] = $email_field;
+	$fields['comment'] = $comment_field;
+	$fields['url'] = $url_field;
+	// done ordering, now return the fields:
+	return $fields;
+}
