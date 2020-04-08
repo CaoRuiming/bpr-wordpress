211a6f587c9bd1b4d0134b762d518f3cb0d9b5dd
Nick Young <youngnicholas228@gmail.com>
2020-04-07 21:15:29 -0700

    functions.php cleanup, css style

--- wp-content/themes/bpr2018/functions.php
+++ wp-content/themes/bpr2018/functions.php
@@ -100,10 +100,10 @@ function mo_comment_fields_custom_html( $fields ) {
     unset( $fields['url'] );
     // then re-define them as needed:
     $fields = [
-        'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name:', 'textdomain'  ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
-        '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" maxlength="245"' . $aria_req . $html_req . ' /></p>',
-        'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email:', 'textdomain'  ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
-        '<input id="email" name="email" ' . ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" maxlength="100" aria-describedby="email-notes"' . $aria_req . $html_req  . ' /></p>',
+        'author' => '<p class="comment-form-author"><label for="author">' . __( 'Name:', 'textdomain'  ) . ( ' <span class="required">*</span>' ) . '</label> ' .
+        '<input id="author" name="author" type="text" size="30" maxlength="245"' . ' /></p>',
+        'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email:', 'textdomain'  ) . ( ' <span class="required">*</span>' ) . '</label> ' .
+        '<input id="email" name="email" ' . ( 'type="email"' ) . ' size="30" maxlength="100" aria-describedby="email-notes"/></p>',
         'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment:', 'noun', 'textdomain' ) . '</label> ' .
             '<textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" aria-required="true" required="required" value="<?echo esc_html(comment[\'comment\']); ?>"></textarea></p>',
     ];
