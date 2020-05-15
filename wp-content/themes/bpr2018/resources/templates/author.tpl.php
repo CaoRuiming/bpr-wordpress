2ebe8eb55aa11345d94af1aca04e3fb2c90f2ff2
CaoRuiming <rcao22556@gmail.com>
2020-05-15 18:12:47 -0400

    added possible fix for blurry gravatars

--- wp-content/themes/bpr2018/resources/templates/author.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/author.tpl.php
@@ -5,7 +5,7 @@
     <div class="col-md-3 col-xs-12">
       <div id="author-avatar-placeholder">
         <div id="author-avatar" style="background-image:
-          url(<?php echo nl2br(get_avatar_url(get_the_author_meta('user_email'))); ?>)">
+          url(<?php echo nl2br(get_avatar_url(get_the_author_meta('user_email'), array("size" => 175))); ?>)">
         </div>
       </div>
     </div>
