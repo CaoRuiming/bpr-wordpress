5d5a44b696bdb9bced3d8a450703e7f6e3a037ff
parker <parker_simon@brown.edu>
2021-03-08 18:03:27 -0500

    fixed col-block article titles to not go past the images

--- wp-content/themes/bpr2018/resources/templates/partials/post/col-block.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/partials/post/col-block.tpl.php
@@ -19,9 +19,11 @@ $pic_title = get_the_title($pic_id);
   </a>
 
   <div class="post-title-small">
-    <a itemprop="url" href="<?php echo esc_url(get_permalink()); ?>">
-      <span itemprop="headline"><?php the_title(); ?></span>
-    </a>
+    <div class="title-width-limiter">
+      <a itemprop="url" href="<?php echo esc_url(get_permalink()); ?>">
+        <span itemprop="headline"><?php the_title(); ?></span>
+      </a>
+    </div>
   </div>
 
   <div class="post-author post-date font-size-18">
