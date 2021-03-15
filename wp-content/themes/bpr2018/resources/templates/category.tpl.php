7a547f598dac2ea83a0e43bebb3c63247a566139
parker <parker_simon@brown.edu>
2021-03-15 19:46:35 -0400

    Fixed titles so html having double quotes wont mess with things

--- wp-content/themes/bpr2018/resources/templates/category.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/category.tpl.php
@@ -50,7 +50,7 @@ if (is_single()) {
               <div class="row">
                 <article class="row featured-post">
                   <div class="col-sm-6">
-                    <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo get_the_title($id); ?>">
+                    <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo htmlentities(get_the_title($id), ENT_QUOTES | ENT_SUBSTITUTE); ?>">
                       <div
                         class="img-40"
                         style="background-image: url(<?php echo $pic_url; ?>);">
