f4a3bf2754605a7636c741ccfd2c3e9ecce845ea
Ashley Cai <ashley.lcai@gmail.com>
2021-03-22 19:38:24 -0400

    turned div into image and added invisible text (#72)

--- wp-content/themes/bpr2018/resources/templates/404.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/404.tpl.php
@@ -1,15 +1,13 @@
 <?php get_header(); ?>
 
+<h1 class="sr-only">Sorry, this page doesn't seem to exist!</h1>
 <div class="page-template content-wrapper container-fluid" style="margin-top:50px">
   <?php 
   $pic_url = get_template_directory_uri() . '/resources/assets/images/404.jpg';;
   if ($pic_url):
   ?>
     <div class="row">
-      <div
-        class="featured-image"
-        style="background-image: url(<?php echo $pic_url; ?>);">
-      </div>
+    <img src="<?php echo $pic_url; ?>" class="featured-image" alt="Cat unsuccessfully hiding under a stack of papers">
     </div>
   <?php endif; ?>
 </div>
