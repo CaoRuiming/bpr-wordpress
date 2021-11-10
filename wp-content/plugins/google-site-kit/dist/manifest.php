5f08d5b26a803388ec6fa9dafc8b1418c3962ace
Nick Young <youngnicholas228@gmail.com>
2021-11-10 09:42:26 -0500

    new plugins (#104)

---
+++ wp-content/plugins/google-site-kit/dist/manifest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package   Google\Site_Kit
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+return array(
+	"googlesitekit-admin-css"                    => "/assets/css/googlesitekit-admin-css-31f7ba650e3c97004e23.css",
+	"googlesitekit-adminbar-css"                 => "/assets/css/googlesitekit-adminbar-css-54736a1183630df52f27.css",
+	"googlesitekit-wp-dashboard-css"             => "/assets/css/googlesitekit-wp-dashboard-css-82e90038a81194d16266.css",
+	"googlesitekit-vendor"                        => "googlesitekit-vendor-0b76ec563ac6f603e46d.js",
+	"runtime"                                     => "runtime-f0f21658d19c77437ec7.js",
+	"googlesitekit-activation"                    => "googlesitekit-activation-094808bcb407db5874c1.js",
+	"googlesitekit-adminbar"                      => "googlesitekit-adminbar-eb64b38ff405b666b1d0.js",
+	"googlesitekit-api"                           => "googlesitekit-api-b815e75ca82d700ccbd9.js",
+	"googlesitekit-base"                          => "googlesitekit-base-ad0310171444ef5fa170.js",
+	"googlesitekit-dashboard"                     => "googlesitekit-dashboard-050ebd27abe80f303c1b.js",
+	"googlesitekit-dashboard-details"             => "googlesitekit-dashboard-details-8c579e76c7c547f37153.js",
+	"googlesitekit-dashboard-splash"              => "googlesitekit-dashboard-splash-8dc50beed7d89051291b.js",
+	"googlesitekit-data"                          => "googlesitekit-data-c49913b3834ecdf26c35.js",
+	"googlesitekit-datastore-forms"               => "googlesitekit-datastore-forms-6a8865870850a3db7026.js",
+	"googlesitekit-datastore-location"            => "googlesitekit-datastore-location-6967e1c52deba55b1209.js",
+	"googlesitekit-datastore-site"                => "googlesitekit-datastore-site-6c8cfc107aca28082d0a.js",
+	"googlesitekit-datastore-ui"                  => "googlesitekit-datastore-ui-cfde19b508260dbace41.js",
+	"googlesitekit-datastore-user"                => "googlesitekit-datastore-user-d368f81d73f56ff27405.js",
+	"googlesitekit-idea-hub-notice"               => "googlesitekit-idea-hub-notice-2a9d218aa5d719929e1c.js",
+	"googlesitekit-idea-hub-post-list"            => "googlesitekit-idea-hub-post-list-ade5d3be13ef92b98e83.js",
+	"googlesitekit-module"                        => "googlesitekit-module-c5e26306be5e2182ac25.js",
+	"googlesitekit-modules"                       => "googlesitekit-modules-35f93e35ab30844aaf7e.js",
+	"googlesitekit-modules-adsense"               => "googlesitekit-modules-adsense-3a41e9795d7c3a0989d3.js",
+	"googlesitekit-modules-analytics"             => "googlesitekit-modules-analytics-e9532cc54a0fa731f0da.js",
+	"googlesitekit-modules-analytics-4"           => "googlesitekit-modules-analytics-4-e272c94f039011431ae9.js",
+	"googlesitekit-modules-idea-hub"              => "googlesitekit-modules-idea-hub-2a150c8639d3aac4240b.js",
+	"googlesitekit-modules-optimize"              => "googlesitekit-modules-optimize-ad38f69719eec165f131.js",
+	"googlesitekit-modules-pagespeed-insights"    => "googlesitekit-modules-pagespeed-insights-04e03d03b121a86b83c1.js",
+	"googlesitekit-modules-search-console"        => "googlesitekit-modules-search-console-6b43c0bbbc7abc301d32.js",
+	"googlesitekit-modules-subscribe-with-google" => "googlesitekit-modules-subscribe-with-google-f89a266522d28deb9bb8.js",
+	"googlesitekit-modules-tagmanager"            => "googlesitekit-modules-tagmanager-4715ec22711c74d8ebdf.js",
+	"googlesitekit-polyfills"                     => "googlesitekit-polyfills-cb30c09f310990119dfb.js",
+	"googlesitekit-settings"                      => "googlesitekit-settings-7a266007b3a074a3297f.js",
+	"googlesitekit-user-input"                    => "googlesitekit-user-input-b9dbc12dfef242b64d1d.js",
+	"googlesitekit-widgets"                       => "googlesitekit-widgets-dfe1221206e7052d22b8.js",
+	"googlesitekit-wp-dashboard"                  => "googlesitekit-wp-dashboard-23695002c8444c1f386b.js",
+	"33-dc32fb6a3d05f1480227"                     => "33-dc32fb6a3d05f1480227.js",
+);
