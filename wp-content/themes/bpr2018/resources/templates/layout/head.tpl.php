9e5c03e5579af0d44a195ed4be3035d2c134d510
Nick Young <youngnicholas228@gmail.com>
2022-03-02 19:14:21 -0500

    fixed header

--- wp-content/themes/bpr2018/resources/templates/layout/head.tpl.php
+++ wp-content/themes/bpr2018/resources/templates/layout/head.tpl.php
@@ -121,6 +121,18 @@
           ));
           ?>
         </div>
+        <?php
+        wp_nav_menu(array(
+        'theme_location'    => 'primary',
+        'depth'             => 2,
+        'container'         => 'div',
+        'container_class'   => 'collapse navbar-collapse',
+        'container_id'      => 'navbar-collapse',
+        'menu_class'        => 'nav navbar-nav',
+        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'            => new WP_Bootstrap_Navwalker(),
+        ));
+        ?>
       </nav>
       <script>
         // Allows dropdown parent elements to be clickable links 
